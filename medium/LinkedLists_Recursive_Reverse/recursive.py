ee3ba030a3e2ddc380f6bb3fdab7cd06b82fd194
Pasternak <yuliya.pasternak.2007@gmail.com>
2025-04-04 18:33:04 +0300

    -

--- medium/LinkedLists_Recursive_Reverse/recursive.py
+++ medium/LinkedLists_Recursive_Reverse/recursive.py
@@ -6,9 +6,7 @@ class Node(object):
 def reverse(head):
     if head is None or head.next is None:
         return head
-    
     new_head = reverse(head.next)
     head.next.next = head
     head.next = None
-    
-    return new_head   
+    return new_head
