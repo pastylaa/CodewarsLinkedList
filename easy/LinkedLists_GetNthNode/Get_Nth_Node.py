c4a2acac63af79da7f8e44185433094b585c6e85
Pasternak <yuliya.pasternak.2007@gmail.com>
2025-04-04 17:24:36 +0300

    get nth node task complete

---
+++ easy/LinkedLists_GetNthNode/Get_Nth_Node.py
@@ -0,0 +1,20 @@
+class Node(object):
+    """Node class for reference"""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def get_nth(node, index):
+    if node is None:
+        raise Exception('List is empty')
+
+    current = node
+    count = 0
+
+    while current:
+        if count == index:
+            return current
+        count += 1
+        current = current.next
+
+    raise Exception('Index out of range')
